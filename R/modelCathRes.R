512f3e6f339180aa2969fe42724be825ec505048
Olivier MacBook <olivier.granacher@gmail.com>
2021-08-10 18:18:40 +0000

    1st version of modelCathRes2

---
+++ R/modelCathRes.R
@@ -0,0 +1,50 @@
+#' modelCathoRes1
+#'
+#' Model cathode resistance per groups assuming
+#' the same evolution with age and 0 intercept - using lme4
+#'
+#' @param d data.frame with data
+#' @param group char with group names
+#' @param age cell age
+#' @param res cathode resistance value
+#'
+#' @return summary of model using broom::tidy
+#' @export
+#'s
+#'
+modelCathoRes1 <- function(d, group, age, res) {
+  form <- paste0(names(dplyr::select(d, {{res}})),
+
+                 " ~ log(", names(dplyr::select(d, {{age}})),
+                 ") + ",
+                 names(dplyr::select(d, {{group}})),
+                 " + 0"
+                 )
+  broom::tidy(stats::lm(formula = form, d))
+}
+
+#' modelCathoRes2
+#'
+#' Model cathode resistance per groups assuming
+#' the same evolution with age and 0 intercept - using lme4
+#'
+#' @param d data.frame with data
+#' @param group char with group names
+#' @param age cell age
+#' @param res cathode resistance value
+#'
+#' @return summary of model using broom::tidy
+#' @export
+#'
+#'
+modelCathoRes2 <- function(d, group, age, res) {
+  form <- paste0(names(dplyr::select(d, {{res}})),
+
+                 " ~ 0 + (1 + log(", names(dplyr::select(d, {{age}})),
+                 ") | ",
+                 names(dplyr::select(d, {{group}})),
+                 ") "
+                 )
+  stats::coef(lme4::lmer(form, d))
+}
+
