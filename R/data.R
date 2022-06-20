67c07ca970894789e67734ca07690b083c1ee71b
Granacher <olivier.granacher@trimet.fr>
2022-06-20 15:58:43 +0200

    Mise à jour doc

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Sample Cathode Resistance Data
+#'
+#' A dataset containing cathode resistance with age for 4 groups of pots
+#'
+#' @format A data frame with 13938 rows and 4 variables:
+#' \describe{
+#'   \item{group}{pot group}
+#'   \item{cod_cuve}{pot id}
+#'   \item{agebsq}{pot age in days}
+#'   \item{rucv}{cathode resistance in micro ohm}
+#'   ...
+#' }
+#
+"resCathData"
